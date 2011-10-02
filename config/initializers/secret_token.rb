8dc26f94fa41fa9f092632bf5ead5dea2da29186
Luke Woodard <lukeosu@gmail.com>
2011-10-02 13:42:26 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sportspicks::Application.config.secret_token = '9dc9962621ec0ae1a71149ff8f7b087f14184a858e3b5d2516cb6368e07f77933a1201b21179aff0644a9bb32ee7ba29d8aa481a54dc536a7b480dd23b74d559'
